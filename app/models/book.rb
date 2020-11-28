7711d07410a6f06535a7817a95f00888a87c6845
Kevser Buyukaydin <kevserbuyukaydin@gmail.com>
2020-11-28 20:49:59 +0300

    Add category model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :trades, dependent: :destroy
     belongs_to :user
+    belongs_to :category
     scope :listed, -> {where(status: true)}
 
     validates :title, presence: true 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_29_203949) do
+ActiveRecord::Schema.define(version: 2020_11_28_172351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2020_10_29_203949) do
     t.boolean "status"
     t.string "user_id"
     t.boolean "barter_status"
+    t.integer "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "comments", force: :cascade do |t|
