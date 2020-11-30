3ef219c115f0c195ad505e36e7e7d9cc3f8e7e3c
Kevser Buyukaydin <kevserbuyukaydin@gmail.com>
2020-12-01 02:57:37 +0300

    Add error messages when adding a new category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   has_many :books
+   
+  validates :name, :presence => true
+  validates :name, :uniqueness => { :message => "already registered" }
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
